6ffde50303179f732b2c248d5f875c0884e368fc
Furkan Avcı <furkanavci039@gmail.com>
2023-12-30 14:26:21 +0300

    test route removed

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const test = require('./routes/test')
 const { ApolloServer, gql } = require('apollo-server-express')
 const { faker } = require('@faker-js/faker')
-
-const app = express()
 
 const typeDefs = gql`
   type Person {
@@ -51,8 +48,8 @@ const resolvers = {
 }
 const server = new ApolloServer({ typeDefs, resolvers })
 
+const app = express()
 app.use(express.json())
-app.use('/test', test)
 
 const graphqlPort = process.env.GRAPHQL_PORT ?? 3001
 server.start().then(() => {
